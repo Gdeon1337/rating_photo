d6280c8fd20aa97352c97f03bd38b1f32dca6adf
v.maleev <v.maleev@fun-box.ru>
2020-08-27 17:20:28 +0400

    Loads.

--- app/blueprints/answers/view.py
+++ app/blueprints/answers/view.py
@@ -9,7 +9,7 @@ from app.helpers.swagger import models as swagger_models
 from sanic_openapi import doc  # pylint: disable=wrong-import-order
 from sanic_jwt.decorators import inject_user, protected
 from app.helpers.validators import raise_if_empty, raise_if_not_int, raise_if_not_float
-from app.helpers.loaders import limit_query, admin_load_photos
+from app.helpers.loaders import admin_load_photos, load_photos, load_users
 
 blueprint = Blueprint('photo', url_prefix='/photo', strict_slashes=True)
 
@@ -30,16 +30,14 @@ async def get_photos(request: Request, user: User):
     if user.login == 'admin':
         photos_loads = admin_load_photos(limit=limit, offset=offset)
         photos = await photos_loads.all()
+        photos = [photo.to_dict() for photo in photos]
+        users = await load_users().all()
+        users = [user.to_dict() for user in users]
+        photos = {'photos': photos, 'users': users}
     else:
-        alias_ass = Assessment.alias('ass')
-        subq = db.select([Assessment.photo_id]).where(Assessment.user_id == user.id).as_scalar()
-        subq1 = db.select([Assessment.user_id]).where(Assessment.id == alias_ass.id).as_scalar()
-        query = Photo.query.where(not_(Photo.id.in_(subq)))
-        query = db.select([Photo]).select_from(Photo.outerjoin(alias_ass, or_(and_(alias_ass.user_id == user.id, alias_ass.photo_id == Photo.id), alias_ass.photo_id == None))).\
-                    where(not_(tuple_(user.id).in_(subq1)))
-        photos = limit_query(query, limit, offset)
-        photos = await photos.gino.all()
-    photos = [{'id': str(photo.id), 'path': photo.path} for photo in photos]
+        photos_loads = load_photos(user=user, limit=limit, offset=offset)
+        photos = await photos_loads.all()
+        photos = [{'id': str(photo.id), 'path': photo.path} for photo in photos]
     return json(photos)
 
 
--- app/helpers/loaders.py
+++ app/helpers/loaders.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List, Optional, Union
 
 from gino.api import GinoExecutor
-from sqlalchemy.sql import Select, and_, func, or_
+from sqlalchemy.sql import Select, and_, func, or_, not_, tuple_
 
 from database import User, db, Photo, Assessment
 from database.models.db import CRUDModel
@@ -21,8 +21,25 @@ def limit_query(query: Select, limit: Optional[str] = None, offset: Optional[str
 
 
 def admin_load_photos(limit: Optional[str] = None, offset: Optional[str] = None):
-    query = db.select((Photo, Assessment, User)).select_from(Photo.outherjoin(Assessment.outherjoin(User)))
+    alias_ass = Assessment.alias('ass')
+    subq = db.select([Assessment]).where(Assessment.photo_id == alias_ass.photo_id).as_scalar()
+    query = db.select((Photo, subq)).select_from(Photo.outherjoin(alias_ass))
     query = limit_query(query, limit, offset)
 
-    return query.gino.load(Photo.load(assessments=Assessment.load(users=User)))
+    return query.gino.load(Photo.load(assessments=subq))
+
+
+def load_users():
+    return User.query.where(User.login != 'admin').gino
+
+
+def load_photos(user: User, limit: Optional[str] = None, offset: Optional[str] = None):
+    alias_ass = Assessment.alias('ass')
+    subq = db.select([Assessment.user_id]).where(Assessment.id == alias_ass.id).as_scalar()
+    query = db.select([Photo]).select_from(Photo.outerjoin(alias_ass, or_(
+        and_(alias_ass.user_id == user.id, alias_ass.photo_id == Photo.id), alias_ass.photo_id == None))). \
+        where(not_(tuple_(user.id).in_(subq)))
+    photos = limit_query(query, limit, offset)
+
+    return photos.gino
 
